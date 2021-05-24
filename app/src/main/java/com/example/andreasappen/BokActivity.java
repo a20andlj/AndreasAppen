141e5341b4dd897bf9950364b1c57d93905a55c9
Andreas <a20andlj@student.his.se>
2021-05-24 09:22:11 +0200

    Lade till tillbakabutton på BokActivity

--- app/src/main/java/com/example/andreasappen/BokActivity.java
+++ app/src/main/java/com/example/andreasappen/BokActivity.java
@@ -3,6 +3,8 @@ package com.example.andreasappen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BokActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class BokActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bok);
+
+        Button close = findViewById(R.id.close_bok_activity);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
