a73e801e7613912b2e927b98f71587bff0a57f5c
Andreas <a20andlj@student.his.se>
2021-05-24 10:05:59 +0200

    Lade till en javaclass Books

---
+++ app/src/main/java/com/example/andreasappen/Books.java
@@ -0,0 +1,29 @@
+package com.example.andreasappen;
+
+public class Books {
+
+
+
+    /*
+    "ID": "Mobprog_Dansmusen",
+	"name": "Dansmusens sista dans",
+	"type": "a20andlj",
+	"company": "SAZ AB",
+	"location": "Bokhandel",
+	"category": "Barnbok",
+	"size": 2014,
+	"cost": 129,
+	"auxdata": null
+    */
+
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private Integer size;
+    private Integer cost;
+
+
+}
